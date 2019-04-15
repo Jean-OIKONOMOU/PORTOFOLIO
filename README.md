a416b57e718dcb30567298f60b4377cd7ff0418e
Jean-Adrian OIKONOMOU <Jean.oikonomou@gmail.com>
2019-04-15 15:53:12 +0200

    placeholder

---
+++ README.md
@@ -0,0 +1,14 @@
+# Mon PORTOFOLIO
+Crée dans le but de me faire un portofolio. Pour le moment il n'y a pas d'informations pertinentes.
+
+# Technologies utilisées
+Ce site est fait avec du Jquery, CSS, HTML, et des tas de plugins ! Voici une liste des plugins utilisés:
+- (Typed.js)[https://github.com/mattboldt/typed.js/]
+- (superslides.js)[https://github.com/nicinabox/superslides]
+- (easyPieChart.js)[https://github.com/rendro/easy-pie-chart]
+- (owlCarousel.js)[https://owlcarousel2.github.io/OwlCarousel2/]
+- (countUp.js)[https://github.com/inorganik/CountUp.js]
+- (fancybox.js)[http://fancyapps.com/fancybox/3/]
+- (isotop.js)[https://isotope.metafizzy.co/]
+
+## &copy; (Jean-Adrian OIKONOMOU)[https://github.com/Jean-OIKONOMOU]
---
+++ js/script.js
@@ -0,0 +1,144 @@
+$(window).on("load", function() {
+    $(".loader .inner").fadeOut(1000, function () {
+      $(".loader").fadeOut(250);
+    });
+
+    $(".items").isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false,
+    }
+  })
+})
+
+function load() {
+        $(document).ready(function(){
+          $(this).scrollTop(0);
+        });
+
+        console.error("DOM STATUS: NOMINAL. WELCOME, DAVE.");
+        $('#slides').superslides({
+          animation_speed: 1300,
+          animation: 'fade',
+          pagination: false,
+          play: 3500,
+        });
+
+        var typed = new Typed(".typed", {
+          strings: ["Sample Text", "I'm a Web Developer.", "I'm a Fun Guy.", "I'm an IT Technician.", "I'm a Tech Enthusiast.", "I'm a BeCode Student."],
+          typeSpeed: 100,
+          loop: true,
+          loopCount: Infinity,
+          smartBackspace: true,
+          showCursor: true,
+          autoInsertCss: true,
+          backDelay: 500,
+          backSpeed: 100,
+          startDelay: 0,
+        });
+
+        $('.owl-carousel').owlCarousel({
+          items: 4,
+          responsive:{
+              0:{
+                  items:1.5
+              },
+              480:{
+                  items:2.5
+              },
+              768:{
+                  items:3.5
+              },
+              938:{
+                  items:4
+              }
+            }
+        });
+
+          var skillsTopOffset = $('.skillsSection').offset().top;
+          $(window).scroll(function() {
+            if(window.pageYOffset > (skillsTopOffset - $(window).height() + 300)) {
+              $('.chart').easyPieChart({
+                    easing: 'easeInOut',
+                    barColor: '#fff',
+                    trackColor: false, // it's the color of the empty part of the chart.
+                    scaleColor: false, //color of the linemarks on the charts
+                    lineWidth: 4,
+                    size: 152,
+                    onStep: function(from, to, percent) {
+                      $(this.el).find('.percent').text(Math.round(percent));
+                   }
+                });
+            }
+          });
+
+          var statsTopOffset = $('.statsSection').offset().top;
+          var countUpFinished = false;
+          $(window).scroll(function() {
+            if(!countUpFinished && window.pageYOffset > (statsTopOffset - $(window).height() + 300)) {
+              $(".counter").each(function() {
+                var element = $(this);
+                var endVal = parseInt(element.text());
+                element.countup(endVal);
+              })
+
+              countUpFinished = true;
+            }
+          });
+
+      $("[data-fancybox]").fancybox();
+
+      $("#filters a").click(function() {
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+        $(".items").isotope({
+          filter: selector,
+          animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: true,
+          }
+        });
+        return false;
+      });
+
+      const nav = $("#navigation");
+      const navTop = nav.offset().top;
+
+      $("#navigation li a").click(function(e) {
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({ scrollTop: targetPosition - 79}, "slow");
+      });
+
+
+      $(window).on("scroll", stickyNavigation);
+
+      function stickyNavigation() {
+        var body = $("body");
+        if ($(window).scrollTop() >= navTop) {
+          body.css("padding-top", nav.outerHeight() + "px");
+          body.addClass("fixedNav");
+        } else {
+          body.css("padding-top", 0);
+          body.removeClass("fixedNav");
+        }
+
+      }
+};
+
+function error() {
+  console.error("SOMETHING WENT WRONG WITH THE DOM! RUN FOR YOUR LIVES!");
+}
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", load);
+} else {
+  error();
+}
